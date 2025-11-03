d8d006e3a73b34406c3d4a8ac9ecf2ff1886139d
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2025-11-03 21:00:05 +0500

    first commit

--- myproject/bookshop/views.py
+++ myproject/bookshop/views.py
@@ -9,6 +9,28 @@ def books_list(request):
     books = Book.objects.all()
     return render(request, 'bookshop/books_list.html', {'book': books})
 
+
+
+
+
+
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    return render(request, 'bookshop/book_detail.html', {'book': book})
+    book.number_of_views += 1
+    book.save(update_fields=['number_of_views'])
+    return render(request, 'bookshop/book_detail.html', {'book':book})
+   
+
+
+
+
+
+# def person_list(request):
+#     people = Person.objects.all()
+#     return render(request, 'main/person_list.html', {'people': people})
+
+# def person_detail(request, person_id):
+#     person = get_object_or_404(Person, id=person_id)
+#     return render(request, 'main/person_detail.html', {'person': person})
+
+
