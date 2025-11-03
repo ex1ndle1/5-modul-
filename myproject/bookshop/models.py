d0dd64909b822d0cd62e0972fcb4a3190ab59b42
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2025-11-04 00:29:42 +0500

    first commit

--- myproject/bookshop/models.py
+++ myproject/bookshop/models.py
@@ -3,12 +3,13 @@ from django.utils import timezone
 
 class Book(models.Model):
     title = models.CharField(max_length=129)
-    publishment_date = models.DateField(default=timezone.now)  # дата публикации
-    created_date = models.DateTimeField(auto_now_add=True)      # дата создания записи
+    publishment_date = models.DateField(default=timezone.now)  
+    created_date = models.DateTimeField(auto_now_add=True)      
     price = models.FloatField()
     description = models.CharField(max_length=250)
     author = models.CharField(max_length=30)
     number_of_views = models.IntegerField(default=0)
+    
 
     def __str__(self):
         return self.title
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                
                  'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -116,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
